c05f1c4550d86442f14054bd458b19ebed365de3
Emanuel Bessa <41740085+Emanuelbessa@users.noreply.github.com>
2021-06-29 17:52:51 -0300

    feat(api): :sparkles: migration profissional

---
+++ healthcare-back/database/factories/ProfissionalFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Profissional;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProfissionalFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Profissional::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
