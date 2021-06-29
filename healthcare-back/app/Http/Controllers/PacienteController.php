a6447fa8cb1c5fc5a66aedac76efce8fedd810d5
Emanuel Bessa <41740085+Emanuelbessa@users.noreply.github.com>
2021-06-29 17:42:58 -0300

    feat(api): :sparkles: CRUD paciente

---
+++ healthcare-back/app/Http/Controllers/PacienteController.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\PacienteService;
+use Exception;
+
+class PacienteController extends Controller
+{
+    /**
+     * @var pacienteService
+     */
+    protected $pacienteService;
+
+    /**
+     * PacienteController Constructor
+     *
+     * @param PacienteService $pacienteService
+     *
+     */
+    public function __construct(PacienteService $pacienteService)
+    {
+        $this->pacienteService = $pacienteService;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $result = ['status' => 200];
+
+        try {
+            $result['data'] = $this->pacienteService->getAll();
+        } catch (Exception $e) {
+            $result = [
+                'status' => 500,
+                'error' => $e->getMessage()
+            ];
+        }
+
+        return response()->json($result, $result['status']);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->only([
+            'nome',
+            'email',
+            'telefone',
+        ]);
+
+        $result = ['status' => 200];
+
+        try {
+            $result['data'] = $this->pacienteService->savePacienteData($data);
+        } catch (Exception $e) {
+            $result = [
+                'status' => 500,
+                'error' => $e->getMessage()
+            ];
+        }
+
+        return response()->json($result, $result['status']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+
+        $result = ['status' => 200];
+
+        try {
+            $result['data'] = $this->pacienteService->getById($id);
+        } catch (Exception $e) {
+            $result = [
+                'status' => 500,
+                'error' => $e->getMessage()
+            ];
+        }
+        return response()->json($result, $result['status']);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = $request->only([
+            'nome',
+            'email',
+            'telefone',
+        ]);
+
+        $result = ['status' => 200];
+
+        try {
+            $result['data'] = $this->pacienteService->updatePaciente($data, $id);
+        } catch (Exception $e) {
+            $result = [
+                'status' => 500,
+                'error' => $e->getMessage()
+            ];
+        }
+
+        return response()->json($result, $result['status']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $result = ['status' => 200];
+
+        try {
+            $result['data'] = $this->pacienteService->deleteById($id);
+        } catch (Exception $e) {
+            $result = [
+                'status' => 500,
+                'error' => $e->getMessage()
+            ];
+        }
+        return response()->json($result, $result['status']);
+    }
+}
