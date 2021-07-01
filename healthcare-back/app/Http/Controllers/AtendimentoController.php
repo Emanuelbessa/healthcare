7725dec25ae60d362e3a4aee659da105daab63bf
Emanuel Bessa <41740085+Emanuelbessa@users.noreply.github.com>
2021-07-01 19:01:28 -0300

    feat(api): :sparkles: add atendimento controller with api methods

---
+++ healthcare-back/app/Http/Controllers/AtendimentoController.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\AtendimentoService;
+use Exception;
+
+class AtendimentoController extends Controller
+{
+
+    /**
+     * @var atendimentoService
+     */
+    protected $atendimentoService;
+
+    /**
+     * AtendimentoController Constructor
+     *
+     * @param AtendimentoService $atendimentoService
+     *
+     */
+    public function __construct(AtendimentoService $atendimentoService)
+    {
+        $this->atendimentoService = $atendimentoService;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $result = ['status' => 200];
+
+        try {
+            $result['data'] = $this->atendimentoService->getAll();
+        } catch (Exception $e) {
+            $result = [
+                'status' => 500,
+                'error' => $e->getMessage()
+            ];
+        }
+
+        return response()->json($result, $result['status']);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $result = ['status' => 200];
+
+        try {
+            $result['data'] = $this->atendimentoService->saveAtendimentoData($data);
+        } catch (Exception $e) {
+            $result = [
+                'status' => 500,
+                'error' => $e->getMessage()
+            ];
+        }
+
+        return response()->json($result, $result['status']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $result = ['status' => 200];
+
+        try {
+            $result['data'] = $this->atendimentoService->getById($id);
+        } catch (Exception $e) {
+            $result = [
+                'status' => 500,
+                'error' => $e->getMessage()
+            ];
+        }
+        return response()->json($result, $result['status']);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = $request->all();
+
+        $result = ['status' => 200];
+
+        try {
+            $result['data'] = $this->atendimentoService->updateAtendimento($data, $id);
+        } catch (Exception $e) {
+            $result = [
+                'status' => 500,
+                'error' => $e->getMessage()
+            ];
+        }
+
+        return response()->json($result, $result['status']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $result = ['status' => 200];
+
+        try {
+            $result['data'] = $this->atendimentoService->deleteById($id);
+        } catch (Exception $e) {
+            $result = [
+                'status' => 500,
+                'error' => $e->getMessage()
+            ];
+        }
+        return response()->json($result, $result['status']);
+    }
+
+    /**
+     * Display the specified resource with all Correlations.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function indexatendimentoprocedimentos($id)
+    {
+        $result = ['status' => 200];
+
+        try {
+            $result['data'] = $this->atendimentoService->getWithRelationsById($id);
+        } catch (Exception $e) {
+            $result = [
+                'status' => 500,
+                'error' => $e->getMessage()
+            ];
+        }
+
+        return response()->json($result, $result['status']);
+    }
+
+    /**
+     * Update the Status in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function updatestatusatendimento(Request $request)
+    {
+        $data = $request->all();
+
+        $result = ['status' => 200];
+
+        try {
+            $result['data'] = $this->atendimentoService->updateStatusAtendimento($data);
+        } catch (Exception $e) {
+            $result = [
+                'status' => 500,
+                'error' => $e->getMessage()
+            ];
+        }
+
+        return response()->json($result, $result['status']);
+    }
+}
